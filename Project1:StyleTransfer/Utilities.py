c51700d90f079bc132c6db6a1dd65e502c245e03
Lim Pin <limpin87@gmail.com>
2017-11-05 19:09:10 +0800

    Added Project 1 Style Transfer

---
+++ Project1:StyleTransfer/Utilities.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import PIL.Image
+
+def load_image(filename, max_size=None):
+    image = PIL.Image.open(filename)
+
+    if max_size is not None:
+        ## Scaling doesn't work!
+        factor = float(max_size) / np.max(image.size)
+    
+        # Scale the image's height and width.
+        size = np.array(image.size) * factor
+
+        # Convert back to integers
+        size = size.astype(int)
+
+        # Resize the image.
+        image = image.resize(size, PIL.Image.LANCZOS)
+
+    # Convert to numpy floating-point array.
+    return np.float32(image)
+
+def save_image(image, filename):
+    # Ensure the pixel-values are between 0 and 255.
+    image = np.clip(image, 0.0, 255.0)
+    
+    # Convert to bytes.
+    image = image.astype(np.uint8)
+    
+    # Write the image-file in jpeg-format.
+    with open(filename, 'wb') as file:
+        PIL.Image.fromarray(image).save(file, 'jpeg')
+
+def plot_image_big(image):
+    # Ensure the pixel-values are between 0 and 255.
+    image = np.clip(image, 0.0, 255.0)
+
+    # Convert pixels to bytes.
+    image = image.astype(np.uint8)
+
+    # Convert to a PIL-image and display it.
+    display(PIL.Image.fromarray(image))
+
+def plot_images(content_image, style_image, mixed_image):
+    # Create figure with sub-plots.
+    fig, axes = plt.subplots(1, 3, figsize=(10, 10))
+
+    # Adjust vertical spacing.
+    fig.subplots_adjust(hspace=0.1, wspace=0.1)
+
+    # Use interpolation to smooth pixels?
+    smooth = True
+    
+    # Interpolation type.
+    if smooth:
+        interpolation = 'sinc'
+    else:
+        interpolation = 'nearest'
+
+    # Plot the content-image.
+    # Note that the pixel-values are normalized to
+    # the [0.0, 1.0] range by dividing with 255.
+    ax = axes.flat[0]
+    ax.imshow(content_image / 255.0, interpolation=interpolation)
+    ax.set_xlabel("Content")
+
+    # Plot the mixed-image.
+    ax = axes.flat[1]
+    ax.imshow(mixed_image / 255.0, interpolation=interpolation)
+    ax.set_xlabel("Mixed")
+
+    # Plot the style-image
+    ax = axes.flat[2]
+    ax.imshow(style_image / 255.0, interpolation=interpolation)
+    ax.set_xlabel("Style")
+
+    # Remove ticks from all the plots.
+    for ax in axes.flat:
+        ax.set_xticks([])
+        ax.set_yticks([])
+    
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+
+def visualize_layers(session,model,content_image,layer_ids):
+    feed_dict = model.create_feed_dict(image=content_image)
+
+    # Get references to the tensors for the given layers.
+    layers = model.get_layer_tensors(layer_ids)
+
+    # Calculate the output values of those layers when
+    # feeding the content-image to the model.
+    values = session.run(layers, feed_dict=feed_dict)
+    layer_names = model.get_layer_names(layer_ids)
+
+    for i, layers in enumerate(layer_names):
+        layer_values = values[i]
+        # Plotting only the first 5 Filters
+        filters = layer_values.shape[3]
+        plt.figure(figsize=(20,20))
+        n_columns = 5
+        for i in range(min(5,filters)):
+            plt.subplot(1, n_columns, i+1)
+            plt.title('Filter ' + str(i) + ' at layer: '+ layers)
+            plt.imshow(layer_values[0,:,:,i], interpolation="nearest") #, cmap="gray")
+
+def mean_squared_error(a, b):
+    return tf.reduce_mean(tf.square(a - b))
+
+def create_content_loss(session, model, content_image, layer_ids):
+    """
+    Create the loss-function for the content-image.
+    
+    Parameters:
+    session: An open TensorFlow session for running the model's graph.
+    model: The model, e.g. an instance of the VGG16-class.
+    content_image: Numpy float array with the content-image.
+    layer_ids: List of integer id's for the layers to use in the model.
+    """
+    
+    # Create a feed-dict with the content-image.
+    feed_dict = model.create_feed_dict(image=content_image)
+
+    # Get references to the tensors for the given layers.
+    layers = model.get_layer_tensors(layer_ids)
+
+    # Calculate the output values of those layers when
+    # feeding the content-image to the model.
+    values = session.run(layers, feed_dict=feed_dict)
+
+    # Set the model's graph as the default so we can add
+    # computational nodes to it. It is not always clear
+    # when this is necessary in TensorFlow, but if you
+    # want to re-use this code then it may be necessary.
+    with model.graph.as_default():
+        # Initialize an empty list of loss-functions.
+        layer_losses = []
+    
+        # For each layer and its corresponding values
+        # for the content-image.
+        for value, layer in zip(values, layers):
+            # These are the values that are calculated
+            # for this layer in the model when inputting
+            # the content-image. Wrap it to ensure it
+            # is a const - although this may be done
+            # automatically by TensorFlow.
+            value_const = tf.constant(value)
+
+            # The loss-function for this layer is the
+            # Mean Squared Error between the layer-values
+            # when inputting the content- and mixed-images.
+            # Note that the mixed-image is not calculated
+            # yet, we are merely creating the operations
+            # for calculating the MSE between those two.
+            loss = mean_squared_error(layer, value_const)
+
+            # Add the loss-function for this layer to the
+            # list of loss-functions.
+            layer_losses.append(loss)
+
+        # The combined loss for all layers is just the average.
+        # The loss-functions could be weighted differently for
+        # each layer. You can try it and see what happens.
+        total_loss = tf.reduce_mean(layer_losses)
+        
+    return total_loss
+
+def gram_matrix(tensor):
+    shape = tensor.get_shape()
+    
+    # Get the number of feature channels for the input tensor,
+    # which is assumed to be from a convolutional layer with 4-dim.
+    num_channels = int(shape[3])
+
+    # Reshape the tensor so it is a 2-dim matrix. This essentially
+    # flattens the contents of each feature-channel.
+    matrix = tf.reshape(tensor, shape=[-1, num_channels])
+    
+    # Calculate the Gram-matrix as the matrix-product of
+    # the 2-dim matrix with itself. This calculates the
+    # dot-products of all combinations of the feature-channels.
+    gram = tf.matmul(tf.transpose(matrix), matrix)
+
+    return gram                            
+
+def create_style_loss(session, model, style_image, layer_ids):
+    """
+    Create the loss-function for the style-image.
+    
+    Parameters:
+    session: An open TensorFlow session for running the model's graph.
+    model: The model, e.g. an instance of the VGG16-class.
+    style_image: Numpy float array with the style-image.
+    layer_ids: List of integer id's for the layers to use in the model.
+    """
+
+    # Create a feed-dict with the style-image.
+    feed_dict = model.create_feed_dict(image=style_image)
+
+    # Get references to the tensors for the given layers.
+    layers = model.get_layer_tensors(layer_ids)
+
+    # Set the model's graph as the default so we can add
+    # computational nodes to it. It is not always clear
+    # when this is necessary in TensorFlow, but if you
+    # want to re-use this code then it may be necessary.
+    with model.graph.as_default():
+        # Construct the TensorFlow-operations for calculating
+        # the Gram-matrices for each of the layers.
+        gram_layers = [gram_matrix(layer) for layer in layers]
+
+        # Calculate the values of those Gram-matrices when
+        # feeding the style-image to the model.
+        values = session.run(gram_layers, feed_dict=feed_dict)
+
+        # Initialize an empty list of loss-functions.
+        layer_losses = []
+    
+        # For each Gram-matrix layer and its corresponding values.
+        for value, gram_layer in zip(values, gram_layers):
+            # These are the Gram-matrix values that are calculated
+            # for this layer in the model when inputting the
+            # style-image. Wrap it to ensure it is a const,
+            # although this may be done automatically by TensorFlow.
+            value_const = tf.constant(value)
+
+            # The loss-function for this layer is the
+            # Mean Squared Error between the Gram-matrix values
+            # for the content- and mixed-images.
+            # Note that the mixed-image is not calculated
+            # yet, we are merely creating the operations
+            # for calculating the MSE between those two.
+            loss = mean_squared_error(gram_layer, value_const)
+
+            # Add the loss-function for this layer to the
+            # list of loss-functions.
+            layer_losses.append(loss)
+
+        # The combined loss for all layers is just the average.
+        # The loss-functions could be weighted differently for
+        # each layer. You can try it and see what happens.
+        total_loss = tf.reduce_mean(layer_losses)
+        
+    return total_loss
+
+def create_denoise_loss(model):
+    loss = tf.reduce_sum(tf.abs(model.input[:,1:,:,:] - model.input[:,:-1,:,:])) + \
+           tf.reduce_sum(tf.abs(model.input[:,:,1:,:] - model.input[:,:,:-1,:]))
+
+    return loss
